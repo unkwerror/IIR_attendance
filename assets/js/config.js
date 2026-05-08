3c0c0e21587742418f92668420682ac30c6b3f3f
unkwerror <i.grishanow@gmail.com>
2026-05-08 14:15:31 +0700

    temp: serveo tunnel while DNS propagates

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@
  * Конфигурация приложения и парсинг URL.
  */
 
-export const apiBase = 'https://iir-attendance.netlify.app';
+export const apiBase = 'https://523bfde6b325bc59-109-174-15-132.serveousercontent.com';
 
 /**
  * Параметры из query/hash (iOS Safari QR может отдавать #?...
